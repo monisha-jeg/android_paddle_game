bc658c331c3c09ed0f2c2ad43201291a63060951
kavya <soldatargent@gmail.com>
2017-09-17 16:08:32 +0530

    added abstract object type for paddle, brick and ball to inherit from

---
+++ app/src/main/java/com/example/pop/paddlegame/Object.java
@@ -0,0 +1,51 @@
+package com.example.pop.paddlegame;
+
+import android.graphics.Canvas;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.Shape;
+
+/**
+ * Created by kavya on 17/9/17.
+ */
+
+abstract class Object {
+    Shape shape;
+    int x;
+    int y;
+
+    int speed = 0;
+
+    public Object(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void draw(Canvas canvas){
+        ShapeDrawable shapeDrawable = new ShapeDrawable(shape);
+        shapeDrawable.draw(canvas);
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
