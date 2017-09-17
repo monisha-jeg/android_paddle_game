c607e197c522ea4255be5506a34f7570d9c169c3
kavya <soldatargent@gmail.com>
2017-09-17 15:21:36 +0530

    Revert "added game view to the game activity"
    This reverts commit 01d98c88485856ba0b89ab86bb5d8fff0bf1cd72.

--- app/src/main/java/com/example/pop/paddlegame/GameActivity.java
+++ app/src/main/java/com/example/pop/paddlegame/GameActivity.java
@@ -5,26 +5,9 @@ import android.os.Bundle;
 
 public class GameActivity extends AppCompatActivity {
 
-    private GameView gameView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        gameView = new GameView(this);
-
-        setContentView(gameView);
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        gameView.pause();
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-        gameView.resume();
+        setContentView(R.layout.activity_game);
     }
 }
