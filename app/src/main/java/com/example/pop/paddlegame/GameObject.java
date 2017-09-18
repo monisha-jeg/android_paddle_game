424f1fd7d3df3d2e9e890b8707ae198a3992c152
kavya <soldatargent@gmail.com>
2017-09-18 11:12:52 +0530

    modified abstract class Game Object as per the new tutorial website

--- app/src/main/java/com/example/pop/paddlegame/GameObject.java
+++ app/src/main/java/com/example/pop/paddlegame/GameObject.java
@@ -1,51 +1,42 @@
 package com.example.pop.paddlegame;
 
 import android.graphics.Canvas;
+import android.graphics.RectF;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.Shape;
+
+import static android.R.attr.x;
+import static android.R.attr.y;
 
 /**
  * Created by kavya on 17/9/17.
  */
 
 abstract class GameObject {
-    Shape shape;
-    int x;
-    int y;
+    // Representing all objects as rectangles
+    RectF rectF;
+    float speedX = 0;
+    float speedY = 0;
 
-    int speed = 0;
+    float objHeight;
+    float objWidth;
 
-    public GameObject(int x, int y){
-        this.x = x;
-        this.y = y;
+    public GameObject(int height, int width){
+        objHeight = height;
+        objWidth = width;
+        rectF = new RectF();
     }
 
-    public void draw(Canvas canvas){
-        ShapeDrawable shapeDrawable = new ShapeDrawable(shape);
-        shapeDrawable.draw(canvas);
+    public void update(long fps){
+        rectF.left = rectF.left + speedX/fps;
+        rectF.top = rectF.top + speedY/fps;
+        rectF.right = rectF.left + objWidth;
+        rectF.bottom = rectF.top - objHeight;
     }
 
-    public int getX(){
-        return x;
+    public RectF getRectF(){
+        return this.rectF;
     }
 
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
+    public void reset(){};
 }
