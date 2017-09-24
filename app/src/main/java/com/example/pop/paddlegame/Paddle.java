e3e34c4cc9a2e961f277d567a2e09acd75d01847
kavya <soldatargent@gmail.com>
2017-09-24 11:24:03 +0530

    added paddle class

---
+++ app/src/main/java/com/example/pop/paddlegame/Paddle.java
@@ -0,0 +1,24 @@
+package com.example.pop.paddlegame;
+
+import android.graphics.RectF;
+import android.graphics.drawable.shapes.RectShape;
+
+/**
+ * Created by kavya on 17/9/17.
+ */
+
+public class Paddle extends GameObject {
+    public Paddle(int screenWidth, int screenHeight){
+        super(20, 130);
+        float left= screenWidth/2;
+        float top= screenHeight - 30;
+
+        rectF = new RectF(left, top, left + objWidth, top + objHeight);
+        speedX = 300;
+        speedY = 0;
+    }
+
+    public void bounce(){
+        speedX = -speedX;
+    }
+}
